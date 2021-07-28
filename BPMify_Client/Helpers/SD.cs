c4827371261a7fc77688beadfae915f9017f9f51
Gerwin Weber <gerwin.w@outlook.de>
2021-07-28 15:20:51 +0200

    Read repsonse of token request

--- BPMify_Client/Helpers/SD.cs
+++ BPMify_Client/Helpers/SD.cs
@@ -8,5 +8,9 @@ namespace BPMify_Client.Helpers
     public static class SD
     {
         public const string Local_PkceData = "PkceData";
+
+        public const string AuthState_Initialized = "Initialized";
+        public const string AuthState_ReceivedCode = "ReceivedCode";
+        public const string AuthState_ReceivedToken = "ReceivedToken";
     }
 }
